f605804c1becca4a3a768bacf3213712760d8972
forughshabani90 <forugh.shabani.90@gmail.com>
2023-10-28 06:01:16 -0700

    fc

---
+++ src/Calculator.java
@@ -0,0 +1,13 @@
+public class AppOpt {
+    public static void main(String[] args) {
+        Opt opt = new Opt();
+        System.out.println(opt.sum(1, 2));
+        System.out.println(opt.mines(5, 4));
+        System.out.println(opt.multiple(2, 3));
+        System.out.println(opt.division(5, 4));
+        System.out.println(opt.sin(30));
+        System.out.println(opt.cos(45));
+        System.out.println(opt.tan(60));
+        System.out.println(opt.tan(90));
+    }
+}
---
+++ src/Opt.java
@@ -0,0 +1,42 @@
+public class Opt {
+
+    public int sum(int a, int b) {
+        int c = a + b;
+        return c;
+    }
+
+    public int mines(int a, int b) {
+        int c = a - b;
+        return c;
+    }
+
+    public int multiple(int a, int b) {
+        int c = a * b;
+        return c;
+    }
+
+    public int division(int a, int b) {
+        int c = a / b;
+        return c;
+    }
+
+    public double sin(int a) {
+        double c=sin(a);
+        return c;
+    }
+    public double cos(int a){
+        double c=cos(a);
+        return c;
+    }
+
+    public double tan(int a) {
+        double c = tan(a);
+        return c;
+    }
+    public double cot(int a){
+        double c=cot(a);
+        return c;
+
+    }
+}
+
